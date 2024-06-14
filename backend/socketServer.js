7f19b1d17608853b027acda97bf4bdd0a4f6abc7
farmannaseem <farmannaseem789@gmail.com>
2024-06-14 15:59:11 +0530

    Initial commit

---
+++ backend/socketServer.js
@@ -0,0 +1,23 @@
+// socketServer.js
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Socket.io connection handling
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Socket.io server is running on port ${PORT}`);
+});
